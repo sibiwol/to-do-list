57e5425b18521a090c2409c500654dbc44a2fd8f
thinker Ring <69044941+sibiwol@users.noreply.github.com>
2021-02-14 03:24:11 +0900

    배경 사진 교체 코드

--- bg.js
+++ bg.js
@@ -1,8 +1,8 @@
-// 프랑스국립중앙박물관연합
+// 프랑스국립중앙박물관연합 url
 //https://www.photo.rmn.fr/CS.aspx?VP3=SearchResult&VBID=2CMFCI8ABZT12&SMLS=1&RW=1904&RH=851&PN=1#/SearchResult&VBID=2CMFCI8ABZT12&SMLS=1&RW=1904&RH=851&PN=1
 const body = document.querySelector('body');
 
-const IMG_NUMBER = 5
+const IMG_COUNT = 7
 
 function handleImgLoad() {
     console.log('finished loading')
@@ -10,13 +10,14 @@ function handleImgLoad() {
 
 function paintImage(imgNumber) {
     const image = new Image();
-    image.src = `images/${imgNumber + 1}.jpg`
+    image.src = `arts/`
+    image.src = `arts/${imgNumber + 1}.jpg`
     image.classList.add('bgImage')
     body.appendChild(image)
 }
 
 function genRandom() {
-    const number = Math.floor(Math.random() * IMG_NUMBER);
+    const number = Math.floor(Math.random() * IMG_COUNT);
     return number
 }
 
